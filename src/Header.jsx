cbfb8ceed3cfae26fe31a4219fe31db22fa029ca
David Robert <contact@david-robert.fr>
2023-03-24 10:53:44 +0100

    exo

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.lemonde.fr/">Home</a>
+          </li>
+          <li>
+            <a href="https://www.lemonde.fr/">About</a>
+          </li>
+          <li>
+            <a href="https://www.lemonde.fr/">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Images.jsx
@@ -0,0 +1,20 @@
+const Images = () => {
+  return (
+    <div>
+      <img
+        src="https://www.causeur.fr/wp-content/uploads/2021/10/bernard-henri-levy-zemmour-768x466.jpg"
+        alt="image1"
+      />
+      <img
+        src="https://fac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Ffac.2F2020.2F11.2F27.2Fa6634df2-a79f-43d7-bca6-33557c7f87be.2Epng/1200x900/quality/80/crop-from/center/pascal-praud-un-humoriste-de-canal-vire-apres-un-sketch-ose-sur-le-journaliste-de-cnews.jpeg"
+        alt="image2"
+      />
+      <img
+        src="https://img.20mn.fr/srNKih6WR4G_-qfwdzI_-g/768x492_le_philosophe_alain_finkielkraut_le_16_juin_2015_a_paris"
+        alt="image3"
+      />
+    </div>
+  );
+};
+
+export default Images;
